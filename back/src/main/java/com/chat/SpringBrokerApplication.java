a0e5ae5f3daca26e98061cf1264208b3e2e4d174
HassenBenSlima <hassenbenslima@gmail.com>
2018-07-17 00:49:58 +0100

    update backend

--- back/src/main/java/com/chat/SpringBrokerApplication.java
+++ back/src/main/java/com/chat/SpringBrokerApplication.java
@@ -1,5 +1,9 @@
 package com.chat;
 
+/**
+ * 
+ * @author Hassen Ben Slima
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- back/src/test/java/com/chat/SpringBrokerApplicationTests.java
+++ back/src/test/java/com/chat/SpringBrokerApplicationTests.java
@@ -1,4 +1,4 @@
-package spark.fr;
+package com.chat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
